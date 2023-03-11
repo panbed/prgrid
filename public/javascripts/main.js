8940c0316ce3d07a23936ff0f49a500771e99891
Bed Pandey <bed.dev.p@gmail.com>
2023-03-11 05:20:20 -0500

    clear btn & really basic way to change waveforms

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -15,62 +15,132 @@ const pitches = [
     392.00, // G4
     466.16, // Bb4
     523.25  // C5
-];
+]; // todo: mathematically generate these pitches
+// i think i saw something with logarithms but ummm unfortunately
+// i am not the best at math
 
-var context = new AudioContext();
-//var o = context.createOscillator();
-//var g = context.createGain();
+const waveforms = ['square', 'sawtooth', 'sine', 'triangle'];
+var c = new AudioContext();
 var o, g = null;
 
-function playNote(freq, startTime, stopTime) {
-    o = context.createOscillator();
-    //g = context.createGain();
-    o.type = 'square';
-    o.connect(context.destination);
+function playNote(freq, vol, waveform, startTime, stopTime) {
+    o = c.createOscillator();
+    g = c.createGain();
+    o.type = waveform;
+    //o.connect(c.destination);
+    //g.connect(c.destination);
+    o.connect(g);
+    g.connect(c.destination);
     o.frequency.value = freq;
-    //g.connect(context.destination);
+    g.gain.value = vol;
+    //g.gain.setValueAtTime(1, c.currentTime);
     o.start(startTime);
-    //g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 0.25);
+    //g.gain.setTargetAtTime(0, c.currentTime, 0.015);
     o.stop(stopTime);
+    //g.gain.setValueAtTime(1, stopTime - 0.25);
+    //g.gain.linearRampToValueAtTime(0, stopTime);
 }
 
 function createNote(row, noteNum) {
     //console.log('#note' + noteNum);
+    $('#note' + noteNum).data('noteNum', noteNum);
+    $('#note' + noteNum).data('waveform', waveforms[currentwaveForm]);
     $('#note' + noteNum).on('click', function() {
+        console.log($(this).data('noteNum'));
         if ($(this).hasClass('live')) {
-            //console.log('removing live class');
+            selectedNotes.splice(selectedNotes.indexOf(noteNum), 1);
             $(this).removeClass('live');
         }
         else {
-            //console.log('adding live class');
+            $('#note' + noteNum).data('waveform', waveforms[currentwaveForm]);
+            //console.log('updated note waveform thing ' + $('#note' + noteNum).data('waveform'));
+            selectedNotes.push(noteNum);
             $(this).addClass('live');
+            playNote(pitches[row], vol, waveforms[currentwaveForm], c.currentTime, c.currentTime + 0.1);
         }
-        playNote(pitches[row], context.currentTime, context.currentTime + 0.1);
+        
     });
 }
 
+var selectedNotes = [];
+var currentwaveForm = 0;
+var vol = 0.3; // sorry for these global variables lol
 $(function() {
-    var noteNum = 0;
-    for (let row = 15; row >= 0; row--) {
-        for (let column = 15; column >= 0; column--) {
+    let noteNum = 0;
+    
+    for (let row = 15; row >= 0; row--)
+        for (let column = 15; column >= 0; column--)
             createNote(row, noteNum++);
-        }
-    }
 
-    var columnOffset = 0;
-    setInterval(function() {
-        if (columnOffset > 15) {
-            columnOffset = 0;
+    let columnOffset = 0;
+    let activeNotes = [];
+
+    $('#clearbutton').on('click', function() {
+        $('div#grid').children().removeClass('live');
+        //console.log(selectedNotes);
+        selectedNotes = [];
+        //console.log(selectedNotes);
+    });
+
+    $('#waveforms').on('click', function() {
+        currentwaveForm++;
+        if (currentwaveForm > 3) {
+            currentwaveForm = 0;
         }
-        console.log(columnOffset);    
+        //$('#note' + noteNum).data('waveform', waveforms[currentwaveForm]);
+        //$('div#grid').children().not('.live').data('waveform', waveforms[currentwaveForm]);
+
+        //$('div#grid:not([live])').data('waveform', waveforms[currentwaveForm]);
+        //console.log($('div#grid').children().not('.live'));
+        //$('div#grid').children().not('.live').data('waveform', waveforms[currentwaveForm]);
+        // for (let i = 0; i < 256; i++) {
+        //     if (!$('#note' + i).hasClass('live')) {
+        //         $('#note' + i).data('waveform', waveforms[currentwaveForm]);
+        //         console.log($('#note' + i).data('waveform'));
+        //     }
+        // }
+
+        //console.log(waveforms[currentwaveForm]);
+        playNote(pitches[10], vol, waveforms[currentwaveForm], c.currentTime, c.currentTime + 0.5);
+
+    });
+
+    $('#volume').on('input', function() {
+        vol = this.value;
+    });
+    setInterval(function() {
+        if (columnOffset > 15)
+            columnOffset = 0;
+        //console.log(columnOffset);    
         for (let i = 0; i <= 15; i++) {
+            activeNotes.push((columnOffset + (i * 16)));
             
+            // if (selectedNotes.includes($('#note' + (columnOffset + (i * 16))).data('noteNum'))) {
+            //     selectedNotes.push(columnOffset + (i * 16));
+            // }
             if ($('#note' + (columnOffset + (i * 16))).hasClass('live')) {
-                console.log('looking at note: ' + (columnOffset + (i * 16)));
-                playNote(pitches[15 - i], context.currentTime, context.currentTime + 0.150);
+                //console.log('looking at note: ' + (columnOffset + (i * 16)));
+                playNote(pitches[15 - i], vol, $('#note' + (columnOffset + (i * 16))).data('waveform'), c.currentTime, c.currentTime + 0.150);
+                // if (!selectedNotes.includes(columnOffset + (i * 16))) {
+                //     //selectedNotes.push(columnOffset + (i * 16));
+                // }
+                //$('#note' + (columnOffset + (i * 16))).addClass('playing');
+                //console.log($('#note' + (columnOffset + (i * 16))).data('waveform'));
             }
         }
+
+        for (let j = 0; j < 255; j++) {
+            if (activeNotes.includes(j)) {
+                $('#note' + j).addClass('playing');
+            }
+            else {
+                $('#note' + j).removeClass('playing');
+            }
+        }
+        //console.log(activeNotes);
+        //console.log(selectedNotes);
         columnOffset++;
+        activeNotes = [];
     }, 150);
 
 });
