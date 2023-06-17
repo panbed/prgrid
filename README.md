20cccd95260332887a35638de03368a345618a41
Bed Pandey <bed.dev.p@gmail.com>
2023-06-17 06:36:22 -0400

    tabs, save to localstorage, basic loading/saving

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Development files for prgrid
 
 
-<img width="647" alt="prgrid screenshot" src="https://github.com/panbed/prgrid/assets/106042766/465aa295-570d-4d56-b4f3-65644de54061">
+<img width="647" alt="prgrid screenshot" src="prgrid.png">
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -25,10 +25,6 @@ const waveforms = ['square', 'sawtooth', 'sine', 'triangle'];   // allowed wavef
 var c = new AudioContext();
 var o, g = null;
 
-// i dont even know what these do, but maybe ill remember if i keep it here
-// bars = ['', '', '', '', '', '', '', '']
-// var currentBar = 0
-
 function playNote(freq, vol, waveform, startTime, stopTime) {
     o = c.createOscillator();
     g = c.createGain();
@@ -47,9 +43,24 @@ function playNote(freq, vol, waveform, startTime, stopTime) {
     //g.gain.linearRampToValueAtTime(0, stopTime);
 }
 
+// set a note to be active, 1 arg for just the notenum, 2 for notenum + waveform
+function makeNoteActive() {
+    let noteNum = arguments[0];
+    let noteWaveform = "";
+    if (arguments.length == 1) {
+        noteWaveform = waveforms[currentwaveForm];
+    }
+    else if (arguments.length == 2) {
+        noteWaveform = arguments[1];
+    }
+    $('#note' + noteNum).data('waveform', noteWaveform);
+    selectedNotes.push(noteNum);
+    $('#note' + noteNum).addClass('live');
+    $('#note' + noteNum).text(noteWaveform.slice(0,3));
+}
+
 function createNote(row, noteNum) {
-    // add the 'number' of the note (from 0-255) and waveform to the data
-    // element of the note (its helpful to read from later)
+    // add num of note (0-255) & waveform to the data of note
     $('#note' + noteNum).data('noteNum', noteNum);
     $('#note' + noteNum).data('waveform', waveforms[currentwaveForm]);
 
@@ -60,23 +71,82 @@ function createNote(row, noteNum) {
             $(this).removeClass('live');
             $(this).text("");
         }
-        // if the note is not active, then make it active by adding the live class,
-        // reading the current waveform, adding it to the active notes array,
-        // and playing the pitch of the note
+        // else just make it active and then play a note sample
         else {
-            $('#note' + noteNum).data('waveform', waveforms[currentwaveForm]);
-            selectedNotes.push(noteNum);
-            $(this).addClass('live');
-            $(this).text(waveforms[currentwaveForm].slice(0,3));
+            makeNoteActive(noteNum);
             playNote(pitches[row], vol, waveforms[currentwaveForm], c.currentTime, c.currentTime + 0.1);
         }
         
     });
 }
 
+function clearNotes() {
+    $('div#grid').children().removeClass('live');
+    $('div#grid').children().text("");
+    selectedNotes = [];
+}
+
+var currentTab = 1;         // set the current tab and default (1)
 var selectedNotes = [];     // notes that are 'lit up' on the board, none by default
 var currentwaveForm = 0;    // default to square wave (look at const waveforms for reference)
 var vol = 0.3;              // volume var, can set default here
+
+function exportNotes() {
+    let exportedNotes = "";
+    let currentNoteWaveform = "";
+    for (let i = 0; i < selectedNotes.length; i++) {
+        currentNoteWaveform = $('#note' + selectedNotes[i]).data('waveform'); 
+        exportedNotes = exportedNotes.concat(selectedNotes[i] + ',' + currentNoteWaveform + ':');
+    }
+    exportedNotes = exportedNotes.concat('X');
+    return exportedNotes;
+}
+
+function loadNotes(noteString) {
+    let currentNoteString = "";
+    let currentNoteWaveform = "";
+
+    // loop that reads the string
+    for (let i = 0; i < noteString.length; i++) {
+        if (noteString[i] == 'X') break; // leave if we reach an X (end of string)
+        while (noteString[i] != ',') { // read the first data, the note num
+            currentNoteString = currentNoteString.concat(noteString[i++]); // add number to string then inc.
+        }
+        i++; // this fixes things :-) (it skips the ',' in the number before the next while loop)
+
+        while (noteString[i] != ':') { // reads the waveform for the number
+            currentNoteWaveform = currentNoteWaveform.concat(noteString[i++]);
+        }
+
+        // set the note to be active
+        makeNoteActive(currentNoteString, currentNoteWaveform);
+
+        // reset note num and waveform
+        currentNoteString = "";
+        currentNoteWaveform = "";
+
+    }
+}
+
+function changeTab(tab) {
+    currentTab = tab;                                           // set global currentTab to new tab
+    clearNotes();                                               // clear all notes on grid currently
+    loadNotes(localStorage.getItem('tab' + tab + 'data'));      // load notes from the string saved in localstorage
+
+    for (let i = 1; i <= 3; i++) {
+        // change color of tab if its active
+        if (i == tab)
+            $('#tab' + i).addClass('activetab');
+        else
+            $('#tab' + i).removeClass('activetab');
+
+        // make tab color blank if its empty
+        if (localStorage.getItem('tab' + i + 'data') == 'X')
+            $('#tab' + i).addClass('emptytab');
+        else
+            $('#tab' + i).removeClass('emptytab');
+    }
+}
 
 $(function() {
     let columnOffset = 0;
@@ -87,8 +157,9 @@ $(function() {
     for (let row = 15; row >= 0; row--)
         for (let column = 15; column >= 0; column--)
             createNote(row, noteNum++);
-
-    $('#waveforms').text(waveforms[currentwaveForm].slice(0, 4));
+    
+    // slice the waveforms text to only 3 chars
+    $('#waveforms').text(waveforms[currentwaveForm].slice(0, 3));
 
     // toggling the menu button in jquery with a cool(tm) animation
     $('#menuButton').on('click', function() {
@@ -98,9 +169,40 @@ $(function() {
     // clear button logic: remove anything that is active ('live'),
     // reset any text on the notes, and then remove any selected notes
     $('#clearbutton').on('click', function() {
-        $('div#grid').children().removeClass('live');
-        $('div#grid').children().text("");
-        selectedNotes = [];
+        clearNotes();
+    });
+
+    // set the default tab to 1
+    changeTab(1);
+
+    // logic for volume slider
+    $('#volume').on('input', function() {
+        vol = this.value;
+    });
+
+    // logic for the load button and export button
+    $('#loadbutton').on('click', function() {
+        loadNotes($('#loadtext').val());
+    });
+
+    $('#savebutton').on('click', function() {
+        navigator.clipboard.writeText(exportNotes());
+        // todo: make this look cooler
+        console.log("Copied text to clipboard!");
+    });
+
+    $('#grid, #clearbutton, #loadbutton').on('click', function() {
+        switch (currentTab) {
+            case 1:
+                localStorage.setItem('tab1data', exportNotes());
+                break;
+            case 2:
+                localStorage.setItem('tab2data', exportNotes());
+                break;
+            case 3:
+                localStorage.setItem('tab3data', exportNotes());
+                break;
+        }
     });
 
     // logic for the waveform button, displays the current waveform text
@@ -113,23 +215,20 @@ $(function() {
             currentwaveForm = 0;
 
         // display only 4 chars of waveform text, and play an example waveform when cycling through
-        $('#waveforms').text(waveforms[currentwaveForm].slice(0, 4));
+        $('#waveforms').text(waveforms[currentwaveForm].slice(0, 3));
         playNote(pitches[10], vol, waveforms[currentwaveForm], c.currentTime, c.currentTime + 0.5);
 
     });
 
-    // logic for volume slider
-    $('#volume').on('input', function() {
-        vol = this.value;
-    });
+    // amazing tab logic :3 (im so sorry)
+    $('#tab1').on('click', function() {changeTab(1);});
+    $('#tab2').on('click', function() {changeTab(2);});
+    $('#tab3').on('click', function() {changeTab(3);});
 
-    // interval function that acts as the bpm for the piano grid,
-    // controls the 'active' line that plays notes
+    // logic for the 'active' line that plays notes
     setInterval(function() {
         // reset position once we reach the end
-        if (columnOffset > 15) {
-            columnOffset = 0;
-        }
+        if (columnOffset > 15) columnOffset = 0;
 
         for (let i = 0; i <= 15; i++) {
             // add the note num to the 'currently playing' (activeNotes) array
@@ -141,10 +240,7 @@ $(function() {
             }
         }
 
-        // run through the entire grid and add
-        // the playing class to it, meaning that it is currently being played 
-        // (i actually forgot why i even do this, 
-        // i think i wanted to add some feature later but i forgot?)
+        // run through the entire grid and add the playing class to it
         for (let j = 0; j < 255; j++) {
             if (activeNotes.includes(j)) {
                 $('#note' + j).addClass('playing');
@@ -153,13 +249,9 @@ $(function() {
                 $('#note' + j).removeClass('playing');
             }
         }
-
-        // console.log(selectedNotes);
+        
         columnOffset++;     // increment the current column
         activeNotes = [];   // reset the active notes, since we moved to the next column
     }, 150);
 
 });
-
-
-
